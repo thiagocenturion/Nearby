bf3bbb2e77162675702258013f8482c0c3dc9609
Thiago Centurion <thiago.centurion@neon.com.br>
2020-12-12 21:21:22 -0300

    Creating project

---
+++ Nearby/Nearby/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nearby
+//
+//  Created by Thiago Rodrigues Centurion on 12/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
